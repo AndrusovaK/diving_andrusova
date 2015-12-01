72a71c02304331d2d670e802a05ffc7edaefc4f3
AndrusovaK <andrusova_ksenia@mail.ru>
2015-12-01 23:35:08 +0300

    all courses slider

--- js/index.js
+++ js/index.js
@@ -17,20 +17,25 @@
 
 
 //Табы для страницы всех курсов
-$(document).ready(function(){
 
-  $('.tabs a').on('click',function(e){
-    e.preventDefault();
-    $(this).closest('.tabs').find('.tabs__item').removeClass('tabs__item--active');
-    $(this).closest('.tabs__item').addClass('tabs__item--active');
-    
-    var tabs_cnt_id = $(this).closest('.tabs').data('tabs-content-id');
-    $(tabs_cnt_id).find('.tabs-content__item').removeClass('tabs-content__item--active');
-    $(this.hash).addClass('tabs-content__item--active');
-  });
-  
-});
 
 
 //Карусель стр. всех курсов
 
+$('.courses-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: $('.course-prev'),
+  nextArrow: $('.course-next'),
+  responsive: [
+	 {
+      breakpoint: 1900, 
+	  settings: "slick"
+	 },
+	  
+	  {
+      breakpoint: 1219, 
+	  settings: "unslick"
+	}
+  ]
+});
