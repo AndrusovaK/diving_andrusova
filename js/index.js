b5fad1488bf3d83996c4dc771800c6d6ec71eb04
AndrusovaK <andrusova_ksenia@mail.ru>
2015-12-03 19:17:36 +0300

    finished pop up

--- js/index.js
+++ js/index.js
@@ -16,10 +16,6 @@
 		});
 
 
-//Табы для страницы всех курсов
-
-
-
 //Карусель стр. всех курсов
 
 $('.courses-slider').slick({
@@ -83,4 +79,30 @@ $(document).ready(function(){
     }
   });
 
+});
+
+
+
+//Pop up
+
+jQuery(document).ready(function($){
+	//open popup
+	$('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
 });
