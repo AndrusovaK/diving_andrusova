cc48402a5f41d7ed3d75e5d559de70e902717627
AndrusovaK <andrusova_ksenia@mail.ru>
2015-12-07 22:25:47 +0300

    owl carousel is finished
    реализовала отключение карусели на маленьких экранах

--- js/index.js
+++ js/index.js
@@ -16,6 +16,30 @@
 		});
 
 
+//Pop up
+
+jQuery(document).ready(function($){
+	//open popup
+	$('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
 //Карусель на главной с отзывами
 $('#review-carousel').slick({
   slidesToShow: 1,
@@ -45,31 +69,6 @@ $('#review-carousel').slick({
 	  }
 	}
   ]
-});
-
-
-//Pop up
-
-jQuery(document).ready(function($){
-	//open popup
-	$('.cd-popup-trigger').on('click', function(event){
-		event.preventDefault();
-		$('.cd-popup').addClass('is-visible');
-	});
-	
-	//close popup
-	$('.cd-popup').on('click', function(event){
-		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
-			event.preventDefault();
-			$(this).removeClass('is-visible');
-		}
-	});
-	//close popup when clicking the esc keyboard button
-	$(document).keyup(function(event){
-    	if(event.which=='27'){
-    		$('.cd-popup').removeClass('is-visible');
-	    }
-    });
 });
 
 
